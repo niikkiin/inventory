668d37cf680d2ddb2fcb0d703242b1087db208c0
Nikki <abarcanikki@gmail.com>
2020-06-25 11:07:29 +0800

    fixed date error

--- client/src/utilities/date/date.component.jsx
+++ client/src/utilities/date/date.component.jsx
@@ -16,7 +16,7 @@ export function getCurrentDate(separator = ', ', space = ' ') {
 
 	let newDate = new Date();
 	let date = newDate.getDate();
-	let month = newDate.getMonth() + 1;
+	let month = newDate.getMonth();
 	let year = newDate.getFullYear();
 
 	let monthName = monthNames[month];
